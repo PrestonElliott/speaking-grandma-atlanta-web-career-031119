b253dd941b0d71ff2d85a80235f4bd9f5197f0ef
Preston Elliott <preston.elliott24@gmail.com>
2019-03-01 23:36:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,14 @@
 # Write a speak_to_grandma method.
+phrase
+
 def speak_to_grandma(phrase)
   if(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif(phrase.upcase)
     return "NO, NOT SINCE 1938!"
     
-  else return 
+  else return
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
