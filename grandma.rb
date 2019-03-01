5dcaa9d2ce657b16ff9812eedda09d3687bfb230
Preston Elliott <preston.elliott24@gmail.com>
2019-03-01 23:33:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if(phrase.upcase)
- 
+    return 
+    
   else return "HUH?! SPEAK UP, SONNY!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
